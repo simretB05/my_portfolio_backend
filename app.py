2cdf58d1ab9e2b416973cb8d83840a92f4302f24
Simret Paulos <happysimret@gmail.com>
2024-02-15 17:21:48 +0300

    Add endpoint for downloading resume PDF

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify ,send_file
+import os
+from reportlab.pdfgen import canvas
 from flask_cors import CORS
 import dbcreds
 import apiHelper
@@ -20,7 +22,33 @@ def post_new_user_contact():
     if(type(results)==list):
         return make_response(jsonify(results), 200)
     else:
-      return make_response(jsonify(results), 500) 
+      return make_response(jsonify(results), 500)  
+    from flask import Flask, send_file
+import os
+
+app = Flask(__name__)
+
+@app.get('/api/download_resume')
+def download_resume():
+    # Specify the folder and filename
+    folder_name = 'resume_pdf'
+    pdf_filename = 'SimretPaulosResume.pdf'
+
+    # Construct the full path to the PDF file
+    pdf_path = os.path.join(os.getcwd(), folder_name, pdf_filename)
+
+    # Check if the file exists
+    if os.path.exists(pdf_path):
+        # Send the file as a response
+        return send_file(pdf_path, as_attachment=True)
+    else:
+        return "File not found"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
 
 if dbcreds.production_mode:
     print("Running in Production Mode")
